78eec0eb9ef2288be590939a3e33efd8d269f0e6
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-11-19 00:08:54 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+const u = 2
+const v = 80
+
+function add(u, v){
+    return u + v
+}
+
+function subtract(u, v){
+    return u - v
+}
+function multiply(u, v){
+    return u * v
+}
+function divide(u, v){
+    return u / v
+}
+function increment(u){
+    return u += 1
+}
+function decrement(v){
+    return v -= 1
+}
+function makeInt(v) {
+    return parseInt(v, 10);
+}
+function preserveDecimal(u) {
+    return parseFloat(u)
+}
+
+
+
+
